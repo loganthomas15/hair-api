e278286130dc5c0317f1b8c069cd2f05f5385c21
loganthomas15 <lthomas2274@gmail.com>
2025-07-07 15:52:50 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, UploadFile, File
+from transformers import ViTForImageClassification, ViTImageProcessor
+from PIL import Image
+import torch
+
+app = FastAPI()
+
+model = ViTForImageClassification.from_pretrained("lthomas8/hair_type_classifier")
+processor = ViTImageProcessor.from_pretrained("lthomas8/hair_type_classifier")
+id2label = model.config.id2label
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = Image.open(file.file).convert("RGB")
+    inputs = processor(images=image, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+    probs = outputs.logits.softmax(dim=-1)
+    predicted_class_idx = torch.argmax(probs).item()
+    predicted_class_label = id2label[predicted_class_idx]
+    confidence = round(probs[0][predicted_class_idx].item(), 4)
+    return {"label": predicted_class_label, "confidence": confidence}
